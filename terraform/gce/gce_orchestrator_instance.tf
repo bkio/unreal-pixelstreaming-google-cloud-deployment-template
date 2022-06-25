4396777af5f0669715f41549bde1cd6360d39c27
Burak Kara <burak@karasoftware.com>
2022-06-25 18:17:59 +0200

    Fix 45

--- terraform/gce/gce_orchestrator_instance.tf
+++ terraform/gce/gce_orchestrator_instance.tf
@@ -70,7 +70,7 @@ resource "null_resource" "post_orchestrator_vm_creation_build_deploy_dotnet_app"
   }
   provisioner "local-exec" {
     working_dir = var.ORCHESTRATOR_SERVICE_DIRECTORY_FULL_PATH
-    command = <<EOT  
+    command = <<Settings
       echo '${var.GOOGLE_CREDENTIALS}' > credentials.json
       export GOOGLE_APPLICATION_CREDENTIALS=$PWD/credentials.json
 
@@ -85,9 +85,9 @@ resource "null_resource" "post_orchestrator_vm_creation_build_deploy_dotnet_app"
       cd output
 
       gcloud builds submit --project=${var.GOOGLE_CLOUD_PROJECT_ID} --tag=gcr.io/${var.GOOGLE_CLOUD_PROJECT_ID}/${var.ORCHESTRATOR_CONTAINER_NAME}:latest
-      
+
       rm $GOOGLE_APPLICATION_CREDENTIALS
-    EOT
+    Settings
   }
 }
 
