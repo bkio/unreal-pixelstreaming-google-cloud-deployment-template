0a9d27ab7c37568b3c104e438f58c0724eb2f7be
Burak Kara <burak@karasoftware.com>
2023-07-24 22:53:19 +0200

    Nonsensitive flags extended - 6

--- orchestrator-service/services/ServicePixelStreamingOrchestrator/Program.cs
+++ orchestrator-service/services/ServicePixelStreamingOrchestrator/Program.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
-using System.Text;
 using CloudConnectors;
 using CloudServiceUtilities;
 using WebServiceUtilities;
--- terraform/gce/gce_module_vars.tf
+++ terraform/gce/gce_module_vars.tf
@@ -2,55 +2,44 @@
 
 variable "GOOGLE_CLOUD_PROJECT_ID" {
   type = string
-  sensitive = false
 }
 
 variable "GOOGLE_CREDENTIALS" {
   type = string
-  sensitive = false
 }
 
 variable "VM_ZONES" {
   type = list(string)
-  sensitive = false
 }
 
 variable "VM_NAME_PREFIX" {
   type = string
-  sensitive = false
 }
 
 variable "ORCHESTRATOR_VM_INSTALL_SH_FILE_CONTENT" {
   type = string
-  sensitive = false
 }
 
 variable "GPU_VM_INSTALL_SH_FILE_CONTENT" {
   type = string
-  sensitive = false
 }
 
 variable "ORCHESTRATOR_CONTAINER_NAME" {
   type = string
-  sensitive = false
 }
 
 variable "PIXEL_STREAMING_UNREAL_CONTAINER_IMAGE_NAME" {
   type = string
-  sensitive = false
 }
 
 variable "GPU_INSTANCES_PER_ZONE" {
   type = string
-  sensitive = false
 }
 
 variable "MAX_USER_SESSION_PER_INSTANCE" {
   type = string
-  sensitive = false
 }
 
 variable "DOMAIN_NAME" {
   type = string
-  sensitive = false
 }
